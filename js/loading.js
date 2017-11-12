137b483fe56d9ae63b8e6edd06de105b21cdad93
birdycn <32201961+birdycn@users.noreply.github.com>
2017-11-12 13:11:51 +0800

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,14 +1,15 @@
 // 文档结构载入完成后，显示Loading效果
 $(function () {
-	loading_container = '<span id="loading_container" style="position:absolute;z-index:1000;width:100%;height:100%;padding:0;margin:0;text-align:center;background-color:#ffffff;">';
+/* 	loading_container = '<div id="loading_container" style="position:absolute;z-index:1000;width:100%;height:100%;padding:0;margin:0;text-align:center;background-color:#ffffff;">';
 	loading_container += '<div id="loading_circle">';
 	loading_container += '<span id="outer"><span id="inner"></span></span>';
 	loading_container += 'Loading';
 	loading_container += '<div id="loading_slow">网速有点慢，请继续等待或 <a href="#" id="loading_refresh">重载</a> 网页。</div>';
 	loading_container += '<div>已经载入 <span id="loading_percent">0</span>%</div>';
-	loading_container += '</span>';
 	loading_container += '</div>';
-	$("body").prepend(loading_container);
+	loading_container += '</div>';
+	console.log(loading_container);
+	$("body").prepend(loading_container); */
 	setTimeout(function () {
 		$("#loading_slow").fadeIn(500)
 	},
